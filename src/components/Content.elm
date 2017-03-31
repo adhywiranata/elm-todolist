9d55032e02e2e31240e35fb52951960aa949b834
Adhy <adhywiranatap@gmail.com>
2017-03-31 18:45:00 +0700

    added components

--- src/components/Content.elm
+++
@@ -1,9 +0,0 @@
-module Content exposing (..)
-
-import Html exposing (..)
-import Html.Attributes exposing (..)
-
-view : Html msg
-view =
-    div [ class "content" ]
-        [ text "this is the content" ]
--- src/components/Header.elm
+++ src/components/Header.elm
@@ -6,15 +6,15 @@ import Html.Attributes exposing (..)
 view : Html msg
 view =
     div [ class "header" ]
-        [ div [ class "logo" ] [ h1 [] [ text "BikinPakeElm.com" ] ]
+        [ div [ class "logo" ]
+            [ img [ src "http://package.elm-lang.org/assets/elm_logo.svg" ] []
+            , span [] [ text "TodosBikinPakeElm.net" ] ]
         , nav [ class "main-nav" ]
             [ ul []
                 [ a [ href "https://google.com" ]
                     [ li [] [ text "Home" ] ]
                 , a [ href "https://google.com" ]
                     [ li [] [ text "About" ] ]
-                , a [ href "https://google.com" ]
-                    [ li [] [ text "Gallery" ] ]
                 ]
             ]
         ]
